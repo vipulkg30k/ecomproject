df8c5f982f1d0cbdb3f9b410bdfac011cbdf0714
Vipul Goyal <vipulkg30@gmail.com>
2021-03-12 10:34:30 +0530

    ejs start

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  return res.send('hello world')
+  return res.render('index')
 })
 
 app.listen(8085, () => {
