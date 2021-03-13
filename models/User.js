be04d3fad9e83b9520f925c97d687c384bbdb978
Vipul Goyal <vipulkg30@gmail.com>
2021-03-13 11:19:24 +0530

    password encrypted and message error solved

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -28,6 +29,12 @@ const userSchema = mongoose.Schema({
 },
 {
   timestamp: true
+})
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) next()
+  this.password = await bcrypt.hash(this.password, 10)
+  next()
 })
 
 const User = mongoose.model('user', userSchema)
