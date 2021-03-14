92b4846dae19445166cdc050368385735b08f7c1
Vipul Goyal <vipulkg30@gmail.com>
2021-03-14 09:52:58 +0530

    session support added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 require('./utils/db.config')
 
@@ -8,9 +9,19 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 
+// app.set('trust proxy', 1)
+app.use(session({
+  secret: '028951b24e5dc360e65db1711698187102d8becb',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
 app.use('/', authRoutes)
 
 app.get('/', (req, res) => {
+  req.session.views = (req.session.views || 0) + 1
+  console.log(`You have visited ${req.session.views} times`)
   return res.render('index')
 })
 
